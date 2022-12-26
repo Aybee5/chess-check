9e67927fc88bf21c888746055208694c548726e6
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2022-12-26 15:13:09 +0100

    Configure gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,6 +40,15 @@ export default defineNuxtConfig({
           src: "https://www.googletagmanager.com/gtag/js?id=G-5GERBPC6YE",
           async: true,
         },
+        {
+          hid: "gtm",
+          children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','G-5GERBPC6YE');`,
+          type: "text/javascript",
+        },
       ],
     },
   },
